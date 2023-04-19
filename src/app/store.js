5db47ee15858799dfff7c43d6cf4bccc6839bca4
davidko5 <dajan526@gmail.com>
2023-04-19 15:22:57 +0200

    pre part 5 commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import postsReduser from '../features/posts/postsSlice'
+import usersReduser from '../features/users/usersSlice'
 
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postsReduser,
+    users: usersReduser,
+  },
 })
