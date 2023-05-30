33ebcd21dad7baa3394c81bd8f70ec588746eacc
davidko5 <dajan526@gmail.com>
2023-05-30 20:18:04 +0200

    pre part 6(performance and data normalizing) commit

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,31 +1,42 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
   const dispatch = useDispatch()
 
-  const users = useSelector(state => state.users)
+  const users = useSelector((state) => state.users)
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
-  const onAuthorChanged = e => setUserId(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
 
-  const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId))
-      setTitle('')
-      setContent('')
+  const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+
+  const onSavePostClicked = async () => {
+    if(canSave) {
+      try {
+        setAddRequestStatus('pending')
+        await dispatch(addNewPost({title, content, user: userId})).unwrap()
+        setTitle('')
+        setContent('')
+        setUserId('')
+      }
+      catch (err) {
+        console.log('Failed to save the post:', err)
+      }
+      finally {
+        setAddRequestStatus('idle')
+      }
     }
   }
 
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
-
-  const userOptions = users.map( user => (
+  const userOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
@@ -43,8 +54,8 @@ export const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
-        <label htmlFor='postAuthor'>Author:</label>
-        <select id='postAuthor' value={userId} onChange={onAuthorChanged}>
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
           <option value=""></option>
           {userOptions}
         </select>
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
+import { selectAllPosts, fetchPosts } from './postsSlice'
+import { Spinner } from '../../components/Spinner'
 
-export const PostList = () => {
-  const posts = useSelector((state) => state.posts)
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
-
-  const renderedPosts = orderedPosts.map((post) => (
+const PostExcerpt = ({ post }) => {
+  return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
@@ -24,12 +21,36 @@ export const PostList = () => {
         View Post
       </Link>
     </article>
-  ))
+  )
+}
+
+export const PostList = () => {
+  const posts = useSelector(selectAllPosts)
+
+  const dispatch = useDispatch()
+  const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
+
+  useEffect(() => {
+    if (postStatus === 'idle') dispatch(fetchPosts())
+  }, [postStatus, dispatch])
+
+  let content
+  if (postStatus === 'loading') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+    content = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
